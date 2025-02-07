b415e293e170fab4f97a6915c2fb8881b78ecf88
Mohammed <mtajir903@gmail.com>
2025-02-08 00:21:35 +0530

    final touch

--- leetcode/src/components/MainPage.tsx
+++ leetcode/src/components/MainPage.tsx
@@ -5,7 +5,9 @@ import { SideBar } from "./SideBar"
 export function MainPage() {
     return <div className="flex h-full bg-black-300 ">
         <SideBar />
-        <Favorite />
-        <ProblemsList />    
+        <div className="ml-52 flex">
+            <Favorite />
+            <ProblemsList />    
+        </div>
     </div>
 }
--- leetcode/src/components/SideBar.tsx
+++ leetcode/src/components/SideBar.tsx
@@ -5,7 +5,7 @@ import { sidebarOpen } from "../store/sidebarState";
 
 export function SideBar() {
     const [open , setOpen] = useRecoilState(sidebarOpen);
-    return <div className={`${open ? "w-68 h-screen p-6 border-2 border-gray-600 " : "w-fit h-fit p-2 rounded translate-3 fixed" } bg-gray-800 text-white `}>
+    return <div className={`${open ? "w-68 h-screen p-6 border-2 border-gray-600 fixed" : "w-fit h-fit p-2 ml-52 rounded translate-3 absolute" } bg-gray-800 text-white `}>
         <div className="flex items-center justify-between">
         {open && <div className="font-bold text-xl">
             My lists
